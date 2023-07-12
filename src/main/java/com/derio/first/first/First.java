52426ba23fc36bea376e12800e5328bf1d94677f
derio123 <deriomatheus6@gmail.com>
2023-07-12 16:24:44 -0300

    Primeiro Projeto

---
+++ src/main/java/com/derio/first/first/First.java
@@ -0,0 +1,13 @@
+
+package com.derio.first.first;
+
+/**
+ *
+ * @author derio123
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
